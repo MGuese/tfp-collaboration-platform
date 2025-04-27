6619f0d0e67a6a3ba043cc418112f81c2f9005e0
JRehkemper <jannik.rehkemper@gmail.com>
2025-04-27 11:00:54 +0200

    Start with basic Design Mockup

---
+++ src/ui.tfp/color-ideas.md
@@ -0,0 +1,15 @@
+```
+--text: #ecdbfc;
+--background: #0d0a10;
+--primary: #a746ff;
+--secondary: #c50032;
+--accent: #f25b16;
+``` 
+
+```
+--text: #eaeff7;
+--background: #090b11;
+--primary: #5886ea;
+--secondary: #71bde2;
+--accent: #ee1c54;
+```
--- src/ui.tfp/src/app/app.routes.ts
+++ src/ui.tfp/src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { ImageUploadComponent } from './pages/image-upload/image-upload.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
   { path: '', component: LandingPageComponent },
-  { path: 'image-upload', component: ImageUploadComponent }
+  { path: 'image-upload', component: ImageUploadComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'home', component: HomeComponent }
 ];

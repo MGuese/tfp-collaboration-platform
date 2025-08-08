2b08e198444e7299f95210cfb519508053d3fe73
gordo <gbanze@gmx.de>
2025-08-08 18:16:32 +0200

    Controller FromServices

--- tfp-collab-userspace-api/GalleryController.cs
+++ tfp-collab-userspace-api/GalleryController.cs
@@ -3,16 +3,20 @@ using tfp_collab_userspace_api.Authorization;
 using tfp_collab_userspace_domain.Request;
 using tfp_collab_userspace_domain.UseCase;
 
+namespace tfp_collab_userspace_api;
+
 [ApiController]
 [Route("api/[controller]")]
 //[Authorize]
-public class GalleryController (ILogger<GalleryController> logger, ICurrentUserContext currentUserContext)
+public class GalleryController ()
     : ControllerBase
 {
     [HttpPost]
     public async Task<ActionResult<Guid>> Create(
         [FromBody] CreateGalleryRequest request, 
-        [FromServices] ICreateGalleryUseCase createGalleryUseCase)
+        [FromServices] ICreateGalleryUseCase createGalleryUseCase,
+        [FromServices] ILogger<GalleryController> logger, 
+        [FromServices] ICurrentUserContext currentUserContext)
     {
         request.OwnerId = currentUserContext.GetCurrentOwnerId();
         logger.LogDebug("Received request for OwnerId: {RequestOwnerId}, creating Gallery {RequestName}", request.OwnerId, request.Name);
